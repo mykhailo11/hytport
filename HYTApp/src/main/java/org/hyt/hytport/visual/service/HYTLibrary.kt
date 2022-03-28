c5372fdcdacb0daa1dfb141da304941c515d93ae
mykhailo <dasendarec@gmail.com>
2022-03-28 20:32:20 +0300

    audio list filter feature implementation

--- HYTApp/src/main/java/org/hyt/hytport/visual/service/HYTLibrary.kt
+++ HYTApp/src/main/java/org/hyt/hytport/visual/service/HYTLibrary.kt
@@ -2,12 +2,12 @@ package org.hyt.hytport.visual.service
 
 import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.view.MotionEvent
 import android.view.View
-import android.widget.BaseAdapter
-import android.widget.Button
-import android.widget.ImageButton
-import android.widget.ListView
+import android.widget.*
+import androidx.core.widget.addTextChangedListener
 import org.hyt.hytport.R
 import org.hyt.hytport.audio.api.model.HYTAudioModel
 import org.hyt.hytport.audio.factory.HYTAudioFactory
@@ -19,9 +19,11 @@ import org.hyt.hytport.visual.factory.HYTViewFactory
 
 class HYTLibrary : HYTBaseActivity() {
 
-    private lateinit var _list: ListView;
+    private var _list: ListView? = null;
 
-    private lateinit var _switch: Button;
+    private var _switch: Button? = null;
+
+    private var _search: EditText? = null;
 
     private lateinit var _backPlayer: ImageButton;
 
@@ -29,15 +31,22 @@ class HYTLibrary : HYTBaseActivity() {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.hyt_library);
         _list = findViewById(R.id.hyt_songs);
-        _list.divider = null;
-        _list.dividerHeight = 0;
+        _list!!.divider = null;
+        _list!!.dividerHeight = 0;
         _switch = findViewById(R.id.hyt_switch);
         _backPlayer = findViewById(R.id.hyt_player_back);
+        _search = findViewById(R.id.hyt_audio_search);
+        _search!!.setOnFocusChangeListener { view: View, focused: Boolean ->
+            if (!focused){
+                val text: String = _search!!.text.toString();
+                (_list!!.adapter as Filterable).filter.filter(text);
+            }
+        }
         _initControls();
     }
 
     private fun _initControls(): Unit {
-        _switch.setOnClickListener {
+        _switch!!.setOnClickListener {
             val repository: Class<HYTAudioRepository>? = _player.getRepository();
             if (repository != null){
                 when (repository.canonicalName) {
@@ -80,7 +89,7 @@ class HYTLibrary : HYTBaseActivity() {
 
             override fun onReady() {
                 _player.queue {
-                    _list.adapter = HYTViewFactory.getAudioAdapter(context, it) { audio: HYTAudioModel ->
+                    _list!!.adapter = HYTViewFactory.getAudioAdapter(context, it) { audio: HYTAudioModel ->
                         _player.play(audio);
                     };
                     _updateQueue();
@@ -88,7 +97,7 @@ class HYTLibrary : HYTBaseActivity() {
             }
 
             override fun onAddNext(audio: HYTAudioModel) {
-                if (_list.adapter != null){
+                if (_list!!.adapter != null){
                     _updateQueue();
                 }
             }
@@ -103,14 +112,14 @@ class HYTLibrary : HYTBaseActivity() {
     private fun _updateQueue(): Unit {
         val repository: Class<HYTAudioRepository>? = _player.getRepository();
         when (repository!!.canonicalName){
-            HYTBaseAudioRepository::class.java.canonicalName -> _switch.setTextColor(
+            HYTBaseAudioRepository::class.java.canonicalName -> _switch!!.setTextColor(
                 this.getColor(R.color.hyt_text_dark)
             );
-            HYTRemoteAudioRepository::class.java.canonicalName -> _switch.setTextColor(
+            HYTRemoteAudioRepository::class.java.canonicalName -> _switch!!.setTextColor(
                 this.getColor(R.color.hyt_grey)
             );
         }
-        (_list.adapter as BaseAdapter).notifyDataSetChanged();
+        (_list!!.adapter as BaseAdapter).notifyDataSetChanged();
     }
 
 }
