1ad05b86ca7962867e3c31152cf4ccf1e41ec916
mykhailo <dasendarec@gmail.com>
2022-03-30 16:18:57 +0300

    factories were updated

--- HYTApp/src/main/java/org/hyt/hytport/audio/factory/HYTAudioPlayerFactory.kt
+++ HYTApp/src/main/java/org/hyt/hytport/audio/factory/HYTAudioPlayerFactory.kt
@@ -1,15 +1,20 @@
 package org.hyt.hytport.audio.factory
 
 import android.content.Context
+import org.hyt.hytport.audio.api.model.HYTAudioModel
 import org.hyt.hytport.audio.api.service.HYTAudioPlayer
 import org.hyt.hytport.audio.service.HYTBaseAudioPlayer
+import java.util.*
 
 class HYTAudioPlayerFactory {
 
     companion object{
 
-        fun getAudioPlayer(context: Context, completion: () -> Unit): HYTAudioPlayer {
-            return HYTBaseAudioPlayer(context, completion);
+        fun getAudioPlayer(
+            context: Context,
+            queueProvider: ((Deque<HYTAudioModel>) -> Unit) -> Unit
+        ): HYTAudioPlayer {
+            return HYTBaseAudioPlayer(queueProvider, context);
         }
 
     }
