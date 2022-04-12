8215c08bcd1826b75478170e782cdb2a469a2891
mykhailo <dasendarec@gmail.com>
2022-04-12 17:35:49 +0300

    fonts are loaded as constant

---
+++ HYTApp/src/main/java/org/hyt/hytport/visual/component/fonts.kt
@@ -0,0 +1,12 @@
+package org.hyt.hytport.visual.component
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import org.hyt.hytport.R
+
+val fonts: FontFamily = FontFamily(
+    Font(R.font.nunitoregular, FontWeight.Normal),
+    Font(R.font.nunitolight, FontWeight.Light),
+    Font(R.font.nunitoextrabold, FontWeight.Bold)
+);
