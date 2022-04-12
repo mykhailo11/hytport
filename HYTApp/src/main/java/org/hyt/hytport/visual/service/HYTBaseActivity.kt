ba49eddcf2f1726903dd396644458243a5f0c63f
mykhailo <dasendarec@gmail.com>
2022-04-12 17:39:17 +0300

    base activity provides preferences end asynchronous executor

--- HYTApp/src/main/java/org/hyt/hytport/visual/service/HYTBaseActivity.kt
+++ HYTApp/src/main/java/org/hyt/hytport/visual/service/HYTBaseActivity.kt
@@ -11,19 +11,25 @@ import androidx.compose.runtime.*
 import org.hyt.hytport.R
 import org.hyt.hytport.audio.api.service.HYTBinder
 import org.hyt.hytport.audio.service.HYTService
+import java.util.concurrent.Executors
+import java.util.concurrent.ScheduledExecutorService
 
 abstract class HYTBaseActivity : ComponentActivity() {
 
     private var _connection: ServiceConnection? = null;
 
+    private var _executor: ScheduledExecutorService? = null;
+
+    protected lateinit var _preferences: SharedPreferences;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState);
-        val preferences: SharedPreferences = getSharedPreferences(
+        _preferences = getSharedPreferences(
             resources.getString(R.string.preferences),
             Context.MODE_PRIVATE
         );
         val preferenceName = resources.getString(R.string.preferences_permissions);
-        if (!preferences.getBoolean(preferenceName, false)) {
+        if (!_preferences.getBoolean(preferenceName, false)) {
             startActivityIfNeeded(Intent(this, HYTInit::class.java), 100);
             finish();
             return;
@@ -31,25 +37,28 @@ abstract class HYTBaseActivity : ComponentActivity() {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         window.navigationBarColor = getColor(R.color.hyt_transparent);
         volumeControlStream = AudioManager.STREAM_MUSIC;
+        _executor = Executors.newSingleThreadScheduledExecutor();
         val intent: Intent = Intent(this, HYTService::class.java);
         setContent {
             var player: HYTBinder? by remember { mutableStateOf(null) };
-            val connection: ServiceConnection by derivedStateOf {
-                object : ServiceConnection {
+            val connection: ServiceConnection by remember(player) {
+                derivedStateOf {
+                    object : ServiceConnection {
 
-                    override fun onServiceConnected(component: ComponentName?, binder: IBinder?) {
-                        if (binder != null) {
-                            player = binder as HYTBinder?;
+                        override fun onServiceConnected(component: ComponentName?, binder: IBinder?) {
+                            if (binder != null) {
+                                player = binder as HYTBinder?;
+                            }
+                            _connection = this;
                         }
-                        _connection = this;
-                    }
 
-                    override fun onServiceDisconnected(component: ComponentName?) {
-                        unbindService(this);
-                        player = null;
-                        _connection = null;
-                    }
+                        override fun onServiceDisconnected(component: ComponentName?) {
+                            unbindService(this);
+                            player = null;
+                            _connection = null;
+                        }
 
+                    }
                 }
             }
             val bound: Boolean by produceState(
@@ -65,20 +74,21 @@ abstract class HYTBaseActivity : ComponentActivity() {
                 }
             }
             if (bound && player != null) {
-                compose(player!!);
-            }else {
+                compose(player!!, _executor!!);
+            } else {
                 loading();
             }
         }
     }
 
     @Composable
-    protected abstract fun compose(player: HYTBinder);
+    protected abstract fun compose(player: HYTBinder, executor: ScheduledExecutorService);
 
     @Composable
     protected open fun loading(){}
 
     override fun onDestroy() {
+        _executor?.shutdown();
         if (_connection != null) {
             unbindService(_connection!!);
         }
